2ac7ecf53b21c523fbfade709bf479019c9c7167
Archr <archrr00@gmail.com>
2025-05-21 11:03:46 +0000

    add nvim-autopairs plugin

--- init.lua
+++ init.lua
@@ -29,6 +29,12 @@ require("nvim-tree").setup({
 })
 
 
+require("nvim-autopairs")
+
 -- GENERAL VIM CONFIGURATION
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.wo.number = true  -- Show absolute line numbers
+vim.wo.relativenumber = true  -- Show relative line numbers from the cursor's position
+
+vim.o.tabstop = 4  -- Number of spaces that a <TAB> counts for
+vim.o.shiftwidth = 4  -- Number of spaces to use for each step of autoindent
+vim.o.expandtab = true  -- Convert tabs to spaces
