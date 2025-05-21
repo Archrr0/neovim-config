095cb44fd13753420314d666af8f5d3dabc2cde5
Archr <archrr00@gmail.com>
2025-05-21 11:03:10 +0000

    add new mappings for insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,5 @@
-vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc="nvimtree toggle window" })
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'nvimtree toggle window' })
+
+vim.keymap.set('i', '<C-l>', '<Right>', { noremap = true, desc = 'Move cursor right with CTRL+L combination' })
+vim.keymap.set('i', '<C-h>', '<Left>', { noremap = true, desc = 'Move cursor left with CTRL+L combination' })
+vim.keymap.set('i', '<C-e>', '<C-o>$', { noremap = true, desc = 'Move cursor at the end of the line with CTRL+E combination'})
